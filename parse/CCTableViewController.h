792564e88c07fa439f88db6fee4ca7d0cf753225
ashish ranjan <Ranjan@ashishs-MacBook-Air.local>
2014-05-18 12:14:06 -0400

    first commit

---
+++ parse/CCTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCTableViewController.h
+//  parse
+//
+//  Created by ashish ranjan on 5/17/14.
+//  Copyright (c) 2014 University of Cincinnati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCTableViewController : UITableViewController
+
+@end
